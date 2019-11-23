5eb9a327cb5c5e3bb4b4a731b3f1f267f4e9d005
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-11-23 04:00:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr){
+return arr.slice (0 ,2);
+}
+const returnLastTwoDrivers = function(arr){
+ return arr.slice(-2 );
+}
+
+let selectingDrivers=[returnFirstTwoDrivers(),returnLastTwoDrivers()];
+  
+}
